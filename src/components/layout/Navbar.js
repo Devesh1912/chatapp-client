77f529ae3f225786624ea0de74223113af743ad0
Devesh Kumar <deveshs911@gmail.com>
2021-08-15 17:25:48 +0530

    added api link

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
 
   const logout = async () => {
     try {
-      const res = await fetch('http://localhost:5000/logout', {
+      const res = await fetch('https://myapplication19.herokuapp.com/logout', {
         credentials: 'include',
       })
       const data = res.json();
