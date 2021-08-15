ba97a98d12ad31b954b78c767e1cdd608ba10007
Devesh Kumar <deveshs911@gmail.com>
2021-08-15 17:16:21 +0530

    first commit

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,41 @@
+import React, { useContext } from 'react'
+import { UserContext } from "../../UserContext";
+import SignedInMenu from './SignedInMenu';
+import SignedOutMenu from './SignedOutMenu';
+
+const Navbar = () => {
+  const { user, setUser } = useContext(UserContext)
+
+  const logout = async () => {
+    try {
+      const res = await fetch('http://localhost:5000/logout', {
+        credentials: 'include',
+      })
+      const data = res.json();
+      console.log('logout data', data)
+      setUser(null)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  const menu = user?<SignedInMenu logout={logout}/>:<SignedOutMenu/>
+  return (
+    <>
+      <nav className="green">
+        <div className="nav-wrapper">
+          <a href="/" className="brand-logo">VChat</a>
+          <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            {menu}
+          </ul>
+        </div>
+      </nav>
+      <ul class="sidenav" id="mobile-demo">
+        {menu}
+      </ul>
+    </>
+  )
+}
+
+export default Navbar
